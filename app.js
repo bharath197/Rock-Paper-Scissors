1d426e1a7ce02b54d68c11c1cf8641d36b6b81de
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-08 20:30:31 +0530

    add button

--- app.js
+++ app.js
@@ -1,69 +1,135 @@
-function getComputerChoice(){
-    let n = Math.floor(Math.random()*3)+1;
+function getComputerChoice() {
+    let n = Math.floor(Math.random() * 3) + 1;
     // console.log(n)
-    if(n==1){
+    if (n == 1) {
         return 'rock'
     }
-    else if(n==2){
+    else if (n == 2) {
         return 'paper'
     }
-    else{
+    else {
         return 'sciccors'
     }
 }
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    // playerwins
-    if(playerSelection==='rock' && computerSelection==='sciccors'
-    || playerSelection==='sciccors' && computerSelection === 'paper' 
-    || playerSelection === 'paper' && computerSelection === 'rock'){
-        return 'user';
+function playRound(e) {
+
+    let computerSelection = getComputerChoice();
+    let playerSelection = this.value.toLowerCase();
+    console.log(`computer choice is ${computerSelection}`)
+    console.log(`player selection is ${playerSelection}`)
+    // playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === 'rock' && computerSelection === 'sciccors'
+        || playerSelection === 'sciccors' && computerSelection === 'paper'
+        || playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('player wins');
     }
-    else if(playerSelection===computerSelection){
-        return 'draw';
+    else if (playerSelection === computerSelection) {
+        console.log('draw')
     }
-    // computer wins
-   else{
-    return 'computer'
-   }
+
+    else {
+        console.log('computer wins');
+    }
 }
 
-
-// const playerSelection = "scIccors";
-// console.log(computerSelection)
-// const computerSelection = 'paper'
-// console.log(playRound(playerSelection, computerSelection));
+const btns = document.querySelectorAll('button')
 
 
-function game(){
-    let playerScore = 0
-    let compScore = 0
-    for(let i = 0; i<5; i++){
-        const computerSelection = getComputerChoice();
-        let q = prompt('rock, paper or sciccor?')
-        // while (!(q==='rock'||q==='scissor'||q==='paper')){
-        //     prompt('enter again')
-        // }
-        if(playRound(q, computerSelection) === 'user') {
-            playerScore += 1
-            console.log(`you win round ${i+1}`)
-        }
-        else if(playRound(q,computerSelection)==='computer') {
-            compScore += 1
-            console.log(`you lose round ${i+1}`)
-        }
-        else{
-            console.log(`round ${i+1} is drawed`)
-        }
-    }
-    if(playerScore>compScore){
-        console.log('You win the game')
-    }
-    else if(playerScore<compScore){
-        console.log('you lose the game')
-    }
-    else{
-        console.log('game draw')
-    }
-}
-// game();
+btns.forEach((btn) => {
+    btn.addEventListener('click', playRound)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function game(){
+//     let playerScore = 0
+//     let compScore = 0
+//     for(let i = 0; i<5; i++){
+//         const computerSelection = getComputerChoice();
+//         let q = prompt('rock, paper or sciccor?')
+
+//         if(playRound(q, computerSelection) === 'user') {
+//             playerScore += 1
+//             console.log(`you win round ${i+1}`)
+//         }
+//         else if(playRound(q,computerSelection)==='computer') {
+//             compScore += 1
+//             console.log(`you lose round ${i+1}`)
+//         }
+//         else{
+//             console.log(`round ${i+1} is drawed`)
+//         }
+//     }
+//     if(playerScore>compScore){
+//         console.log('You win the game')
+//     }
+//     else if(playerScore<compScore){
+//         console.log('you lose the game')
+//     }
+//     else{
+//         console.log('game draw')
+//     }
+// }
+
