361ef1503454d54e49c59e0961e4ad17e9586826
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-09 01:33:03 +0530

    add reset button

--- app.js
+++ app.js
@@ -14,39 +14,60 @@ function getComputerChoice() {
 function playRound(e) {
     round += 1
     console.log(round)
-    if (round>5) {
+    if (round == 5) {
         declareWinner();
+
+    }
+    else if (round > 5) {
         return
     }
     let computerSelection = getComputerChoice();
     let playerSelection = this.value.toLowerCase();
     // console.log(`computer choice is ${computerSelection}`)
     // console.log(`player selection is ${playerSelection}`)
-    
+
     if (playerSelection === 'rock' && computerSelection === 'sciccors'
         || playerSelection === 'sciccors' && computerSelection === 'paper'
         || playerSelection === 'paper' && computerSelection === 'rock') {
         console.log('player wins')
         playerscore += 1
-        player.textContent = player.textContent.slice(0,-1) + playerscore
+        player.textContent = player.textContent.slice(0, -1) + playerscore
     }
     else if (playerSelection === computerSelection) {
-        console.log('draw')        
+        console.log('draw')
     }
     else {
         console.log('computer wins');
         computerscore += 1
-        comp.textContent = comp.textContent.slice(0,-1) + computerscore
+        comp.textContent = comp.textContent.slice(0, -1) + computerscore
     }
 }
 
-function declareWinner(){
+function declareWinner() {
     const para = document.createElement('p')
+    para.setAttribute('id', 'winner')
     para.textContent = ''
-    if(computerscore>playerscore) para.textContent += `computer wins`
-    else if(playerscore>computerscore) para.textContent += `player wins`
+    if (computerscore > playerscore) para.textContent += `computer wins`
+    else if (playerscore > computerscore) para.textContent += `player wins`
     else para.textContent = 'Match draw'
     div.appendChild(para)
+    reset()
+}
+
+function reset() {
+    const resetbutton = document.createElement('button')
+    resetbutton.textContent = 'reset'
+    div.appendChild(resetbutton)
+    resetbutton.addEventListener('click', () => {
+        round = 0
+        playerscore = 0
+        computerscore = 0
+        comp.textContent = `computer score is 0`
+        player.textContent = `player score is 0`
+        div.removeChild(resetbutton)
+        const para = document.querySelector('#winner')
+        div.removeChild(para)
+    })
 }
 
 let playerscore = 0
@@ -62,7 +83,7 @@ comp.setAttribute('id', 'computer-score')
 comp.textContent = `computer score is 0`
 
 const player = document.createElement('p')
-player.setAttribute('id','player-score')
+player.setAttribute('id', 'player-score')
 player.textContent = `player score is 0`
 
 body.appendChild(div)
